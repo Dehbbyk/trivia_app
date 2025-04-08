a32c2e1c412298eb1fa8d1e3d177f710ea1c4785
Deborah KOLAWOLE <deborah.oyinda@yahoo.com>
2025-04-08 19:58:32 +0100

    Update game_page.dart
    Got trivia questions from the API
    Implemented notify listeners ( listening to provider changes)
    Got answers from the API as well
    Used Alert dialog to display answers
    Implemented end game condition

--- lib/providers/game_page_provier.dart
+++ lib/providers/game_page_provier.dart
@@ -1,12 +1,90 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 
 class GamePageProvider extends ChangeNotifier {
   final Dio _dio = Dio();
+  final int _maxQuestions = 10;
+
+  List? questions;
+  int _currentQuestionCount = 0;
 
   BuildContext context;
   GamePageProvider(this.context) {
     _dio.options.baseUrl = 'https://opentdb.com/api.php';
-    print('Hello');
+    _getQuestionsFromAPI();
+  }
+  Future<void> _getQuestionsFromAPI() async {
+    var response = await _dio.get(
+      '',
+      queryParameters: {
+        'amount': 10,
+        'type': 'boolean',
+        'difficulty': 'easy',
+      },
+    );
+    var data = jsonDecode(
+      response.toString(),
+    );
+    questions = data['results'];
+    notifyListeners();
+  }
+
+  String getCurrentQuestion() {
+    return questions![_currentQuestionCount]['question'];
+  }
+
+  void answerQuestion(String answer) async {
+    bool isCorrect =
+        questions![_currentQuestionCount]['correct_answer'] == answer;
+    _currentQuestionCount++;
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: isCorrect ? Colors.green : Colors.red,
+          title: Icon(
+            isCorrect ? Icons.check_circle : Icons.cancel_sharp,
+            color: Colors.white,
+          ),
+        );
+      },
+    );
+    await Future.delayed(
+      const Duration(seconds: 1),
+    );
+    Navigator.of(context);
+    if (_currentQuestionCount >= _maxQuestions) {
+      endGame();
+    } else {
+      notifyListeners();
+    }
+  }
+
+  Future<void> endGame() async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return const AlertDialog(
+          backgroundColor: Colors.blue,
+          title: Text(
+            'End Game!',
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.white,
+            ),
+          ),
+          content: Text(
+            'Score: 0/0',
+          ),
+        );
+      },
+    );
+    await Future.delayed(
+      const Duration(seconds: 3),
+    );
+    Navigator.pop(context);
+    Navigator.pop(context);
   }
 }
